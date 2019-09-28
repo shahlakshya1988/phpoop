080b25f1af52eb8790af985b5af76ccb47be4d3f
lakshyapaceinfonet <lakshya@paceinfonet.com>
2019-09-28 13:20:22 +0530

    55. The Login Method

--- gallery/admin/includes/session.php
+++ gallery/admin/includes/session.php
@@ -1,6 +1,6 @@
 <?php 
 class Session{
-	private $signed_in;
+	private $signed_in = false;
 	public $user_id;
 
 	public function __construct(){
@@ -8,6 +8,21 @@ class Session{
 			session_start();
 		}
 		$this->check_the_login();
+	}
+
+	public function is_signed_in(){
+		return $this->signed_in;
+	}
+
+	public function login($user){
+		if($user){
+			$this->user_id = $_SESSION["user_id"] = $user->id;
+			$this->signed_in = true;
+		}else{
+			unset($this->user_id);
+			unset($_SESSION["user_id"]);
+			$this->signed_in = false;
+		}
 	}
 
 	private function check_the_login(){
