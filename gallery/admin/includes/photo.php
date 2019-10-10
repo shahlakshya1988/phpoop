9404614d4967958dd1d84b569ec2749b250f72af
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-10-10 07:53:26 +0530

    103. Adding Class Properties

--- gallery/admin/includes/photo.php
+++ gallery/admin/includes/photo.php
@@ -1,5 +1,12 @@
 <?php 
 class Photo extends Db_object{
-	
+	protected static $db_table = "photos";
+	public $photo_id;
+	public $title;
+	public $description;
+	public $filename;
+	public $type;
+	public $size;
+	protected static $db_table_fields = array("title","description","filename","type","size");
 } 
 ?>
