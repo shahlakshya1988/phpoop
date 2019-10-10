135b8073319a0723d6441ee9d66d4fe8c5deaa4c
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-10-10 09:00:31 +0530

    105. Setting Up our Properties Array Course content

--- gallery/admin/includes/photo.php
+++ gallery/admin/includes/photo.php
@@ -8,5 +8,22 @@ class Photo extends Db_object{
 	public $type;
 	public $size;
 	protected static $db_table_fields = array("title","description","filename","type","size");
+
+	public $tmp_path; // this path will be used to move images
+	public $upload_dir = "images"; // this dir is used to store images
+
+	public $upload_errors = array(
+		UPLOAD_ERR_OK => "There is no error",
+		UPLOAD_ERR_INI_SIZE => "The uploaded file exceeds the upload_max_filesize directive",
+		UPLOAD_ERR_FORM_SIZE => "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form. ",
+		UPLOAD_ERR_PARTIAL=>"The uploaded file was only partially uploaded.",
+		UPLOAD_ERR_NO_FILE=>"No file was uploaded.",
+		UPLOAD_ERR_NO_TMP_DIR=>"Missing a temporary folder.",
+		UPLOAD_ERR_CANT_WRITE=>"Failed to write file to disk.",
+		UPLOAD_ERR_EXTENSION=>" A PHP extension stopped the file upload. PHP does not provide a way to ascertain which extension caused the file upload to stop"
+	);
+
+	public $custome_errors = array();
+	
 } 
 ?>
