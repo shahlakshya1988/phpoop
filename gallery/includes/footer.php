8088c12021ec53121aec18c5a99ed05b71b501a3
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-10-17 23:47:58 +0530

    176. Home Page Link and Footer Modifications

--- gallery/includes/footer.php
+++ gallery/includes/footer.php
@@ -5,7 +5,7 @@
         <footer>
             <div class="row">
                 <div class="col-lg-12">
-                    <p>Copyright &copy; Your Website <?php echo date("Y"); ?></p>
+                    <p class="text-center">Copyright &copy; Your Website <?php echo date("Y"); ?></p>
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
