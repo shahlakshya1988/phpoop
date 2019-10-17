fb5de4fb67c2398e802061eda1248093fa229c24
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-10-17 23:29:37 +0530

    175. Coding the PHP in Photo.php FRON-END

--- gallery/photo.php
+++ gallery/photo.php
@@ -31,7 +31,7 @@ $allcomments = Comment::find_the_comments($photo->id);
         <div class="row">
 
             <!-- Blog Post Content Column -->
-            <div class="col-lg-8">
+            <div class="col-lg-12">
 
                 <!-- Blog Post -->
 
@@ -39,9 +39,9 @@ $allcomments = Comment::find_the_comments($photo->id);
                 <h1><?=$photo->title; ?></h1>
 
                 <!-- Author -->
-                <p class="lead">
+                <!-- <p class="lead">
                     by <a href="#">Start Bootstrap</a>
-                </p>
+                </p> -->
 
                 <hr>
 
@@ -51,16 +51,15 @@ $allcomments = Comment::find_the_comments($photo->id);
                 <hr>
 
                 <!-- Preview Image -->
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                <img class="img-responsive" src="admin<?php echo DIRECTORY_SEPARATOR.$photo->picture_path(); ?>" alt="<?php echo $photo->title; ?>">
 
                 <hr>
 
                 <!-- Post Content -->
-                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, doloribus, dolorem iusto blanditiis unde eius illum consequuntur neque dicta incidunt ullam ea hic porro optio ratione repellat perspiciatis. Enim, iure!</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, nostrum, aliquid, animi, ut quas placeat totam sunt tempora commodi nihil ullam alias modi dicta saepe minima ab quo voluptatem obcaecati?</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, dolor quis. Sunt, ut, explicabo, aliquam tenetur ratione tempore quidem voluptates cupiditate voluptas illo saepe quaerat numquam recusandae? Qui, necessitatibus, est!</p>
+                <p class="lead"><?php echo $photo->caption; ?></p>
+               <div>
+				<?php echo $photo->description; ?>
+			   </div>
 
                 <hr>
 
@@ -141,9 +140,9 @@ $allcomments = Comment::find_the_comments($photo->id);
             </div>
 
             <!-- Blog Sidebar Widgets Column -->
-            <div class="col-md-4">
+            <?php /* <div class="col-md-4">
                 <?php include("includes/sidebar.php"); ?>
-            </div>
+            </div> */ ?>
 
         </div>
         <!-- /.row -->
