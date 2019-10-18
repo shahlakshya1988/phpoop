ab78b59693a84a47e84249e741e1e7871e0baaa4
Lakshya Shah <shahlakshya1988@gmail.com>
2019-10-18 13:26:24 +0530

    187. Paginate Indication and Looping

--- gallery/index.php
+++ gallery/index.php
@@ -57,6 +57,16 @@ $photos = Photo::find_by_query($sql);
 					if($paginate->has_previous()){
 						echo "<li class='previous'><a href='".basename($_SERVER["PHP_SELF"])."?page=".$paginate->previous()."'>Previous</a></li>";
 					}
+					for($i = 1; $i<=$paginate->page_total();$i++){
+						if($i==$paginate->current_page){
+							echo "<li class='active'><a href='".basename($_SERVER["PHP_SELF"])."?page=".$i."' >".$i."</a></li>";
+						}else{
+							echo "<li><a href='".basename($_SERVER["PHP_SELF"])."?page=".$i."'>".$i."</a></li>";
+						}
+						
+					}
+					
+					
 					if($paginate->has_next()){
 						echo "<li class='next'><a href='".basename($_SERVER["PHP_SELF"])."?page=".$paginate->next()."'>Next</a></li>";
 					}
