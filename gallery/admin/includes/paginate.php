b3055b6ffe6d78043d82c98882a15433b04e5589
Lakshya Shah <shahlakshya1988@gmail.com>
2019-10-18 10:42:32 +0530

    181. Building our Paginate Class Methods Part # 1

--- gallery/admin/includes/paginate.php
+++ gallery/admin/includes/paginate.php
@@ -9,5 +9,15 @@ class Paginate{
 		$this->items_per_page = (int)$items_per_page;
 		$this->items_total_count = (int)$items_total_count;
 	}
+	
+	public function next(){
+		return $this->current_page +1;
+	}
+	public function previous(){
+		return $this->current_page -1;
+	}
+	public function page_total(){
+		return ceil($this->items_total_count / $this->items_per_page);
+	}
 } 
 ?>
