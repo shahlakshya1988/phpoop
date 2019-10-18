70762deddd05ac3b55d52b311c5c7844e81a620c
Lakshya Shah <shahlakshya1988@gmail.com>
2019-10-18 10:30:38 +0530

    180. Creating our Construct Function to Initialize some Properties

--- gallery/admin/includes/paginate.php
+++ gallery/admin/includes/paginate.php
@@ -1,13 +1,13 @@
 <?php
 class Paginate{
-	public $page;
+	public $current_page;
 	public $items_per_page;
 	public $items_total_count;
 	
 	public function __contruct($page = 1,$items_per_page = 4; $items_total_count = 0){
-		$this->page = $page;
-		$this->items_per_page = $items_per_page;
-		$this->items_total_count = $items_total_count;
+		$this->current_page = (int)$page;
+		$this->items_per_page = (int)$items_per_page;
+		$this->items_total_count = (int)$items_total_count;
 	}
 } 
 ?>
