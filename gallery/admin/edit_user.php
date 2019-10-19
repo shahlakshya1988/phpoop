727733137a0423870a297b09f73b751fe5a25c08
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-10-19 23:27:32 +0530

    191. Creating the Modal + HTML Snippets Download

--- gallery/admin/edit_user.php
+++ gallery/admin/edit_user.php
@@ -1,129 +1,169 @@
 <?php include("includes/header.php"); ?>
 <?php
-if(!$session->is_signed_in()){
+if (!$session->is_signed_in()) {
     redirect("login.php");
     die();
 }
-if(empty(trim($_GET["id"]))){
+if (empty(trim($_GET["id"]))) {
     redirect("users.php");
     die();
 }
 $user = Users::find_by_id($_GET["id"]);
 //var_dump($user);
-if(isset($_POST["edit_user"])){
-  //echo "<pre>",print_r($_POST),"</pre>";
-  //echo "<pre>",print_r($_FILES),"</pre>";
+if (isset($_POST["edit_user"])) {
+    //echo "<pre>",print_r($_POST),"</pre>";
+    //echo "<pre>",print_r($_FILES),"</pre>";
 
     $username = trim($_POST["username"]);
     $first_name = trim($_POST["first_name"]);
     $last_name = trim($_POST["last_name"]);
     $password = trim($_POST["password"]);
-    if(!empty($username)){
+    if (!empty($username)) {
         $user->username = $username;
     }
-    if(!empty($password)){
+    if (!empty($password)) {
         $user->password = $password;
     }
-    if(!empty($first_name)){
+    if (!empty($first_name)) {
         $user->first_name = $first_name;
     }
 
-    if(!empty($last_name)){
+    if (!empty($last_name)) {
         $user->last_name = $last_name;
     }
-    
-   
-    if(empty($_FILES["user_image"])){
+
+
+    if (empty($_FILES["user_image"])) {
         $user->save();
-    }else{
+    } else {
         $user->set_file($_FILES["user_image"]);
         //$user->save_user_and_image();
         $user->upload_photo();
         $user->save();
     }
-    
-    
+
+
     // die();
     redirect("users.php");
 
-	//echo "<pre>",print_r($_REQUEST),"</PRE>";
+    //echo "<pre>",print_r($_REQUEST),"</PRE>";
 
 } // isset($_POST["add_user"])
 
 ?>
-    <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-       <?php  include "includes/top_nav.php"; ?>
-       <?php include "includes/side_nav.php"; ?>
-        <!-- /.navbar-collapse -->
-    </nav>
+<div class="modal fade" id="photo-library">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Gallery System Library</h4>
+            </div>
+            <div class="modal-body">
+                <div class="col-md-9">
+                    <div class="thumbnails row">
 
+                        <!-- PHP LOOP HERE CODE HERE-->
 
-
-    <div id="page-wrapper">
-
-       <div class="container-fluid">
-
-           <!-- Page Heading -->
-           <div class="row">
-               <div class="col-lg-12">
-                   <h1 class="page-header">
-                       Add User
-                   </h1>
-                   <div class="pull-right">
-                       <a href="users.php" class="btn btn-primary">View Users</a>
-                   </div>
-               </div>
-
-                 <form name="add_user" action="" enctype="multipart/form-data" method="POST">
-                  <div class="col-md-5 thumbnail">
-                    <img src="<?php echo $user->image_path_and_placeholder(); ?>" alt="">
-                  </div>
-				 <div class="col-md-7">
-          <div class="form-group">
-            <label for="user_image">User Image</label>
-            <input type="file" name="user_image" id="user_image" class="form-control" >
-          </div>
-						<div class="form-group">
-							<label for="username">Username</label>
-							<input type="text" name="username" class="form-control" value="<?php echo $user->username; ?>" />
+                        <div class="col-xs-2">
+                            <a role="checkbox" aria-checked="false" tabindex="0" id="" href="#" class="thumbnail">
+                                <img class="modal_thumbnails img-responsive" src="<!-- PHP LOOP HERE CODE HERE-->" data="<!-- PHP LOOP HERE CODE HERE-->">
+                            </a>
+                            <div class="photo-id hidden"></div>
                         </div>
-                        <div class="form-group">
-							<label for="password">Password</label>
-							<input type="password" name="password" id="password" class="form-control"  value="<?php echo $user->password; ?>" />
-						</div>
 
-						<div class="form-group">
-						<label for="first_name">First Name</label>
-							<input type="text" name="first_name" id="first_name" class="form-control"  value="<?php echo $user->first_name; ?>" />
-						</div>
-						<div class="form-group">
-							<label for="last_name">Last Name</label>
-							<input type="text" name="last_name" id="last_name" class="form-control" value="<?php echo $user->last_name; ?>"  />
+                        <!-- PHP LOOP HERE CODE HERE-->
+
+                    </div>
+                </div>
+                <!--col-md-9 -->
+
+                <div class="col-md-3">
+                    <div id="modal_sidebar"></div>
+                </div>
+
+            </div>
+            <!--Modal Body-->
+            <div class="modal-footer">
+                <div class="row">
+                    <!--Closes Modal-->
+                    <button id="set_user_image" type="button" class="btn btn-primary" disabled="true" data-dismiss="modal">Apply Selection</button>
+                </div>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div>
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <?php include "includes/top_nav.php"; ?>
+    <?php include "includes/side_nav.php"; ?>
+    <!-- /.navbar-collapse -->
+</nav>
+
+
+
+<div id="page-wrapper">
+
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                    Add User
+                </h1>
+                <div class="pull-right">
+                    <a href="users.php" class="btn btn-primary">View Users</a>
+                </div>
+            </div>
+
+            <form name="add_user" action="" enctype="multipart/form-data" method="POST">
+                <div class="col-md-5 thumbnail">
+                    <a href="#" data-toggle="model" data-target="#photo-library" class="openmodal"><img src="<?php echo $user->image_path_and_placeholder(); ?>" alt=""></a>
+                </div>
+                <div class="col-md-7">
+                    <div class="form-group">
+                        <label for="user_image">User Image</label>
+                        <input type="file" name="user_image" id="user_image" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="username">Username</label>
+                        <input type="text" name="username" class="form-control" value="<?php echo $user->username; ?>" />
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" id="password" class="form-control" value="<?php echo $user->password; ?>" />
+                    </div>
+
+                    <div class="form-group">
+                        <label for="first_name">First Name</label>
+                        <input type="text" name="first_name" id="first_name" class="form-control" value="<?php echo $user->first_name; ?>" />
+                    </div>
+                    <div class="form-group">
+                        <label for="last_name">Last Name</label>
+                        <input type="text" name="last_name" id="last_name" class="form-control" value="<?php echo $user->last_name; ?>" />
+                    </div>
+                    <div class="info-box-footer clearfix">
+                        <a href="delete_user.php?id=<?php echo $user->id ?>" class="btn btn-danger">Delete User</a>
+                        <div class="info-box-update pull-right ">
+                            <input type="submit" name="edit_user" value="Edit User" class="btn btn-primary btn-lg ">
                         </div>
-                        <div class="info-box-footer clearfix">
-                                <a href="delete_user.php?id=<?php echo $user->id ?>" class="btn btn-danger">Delete User</a>
-                                <div class="info-box-update pull-right ">
-                                    <input type="submit" name="edit_user" value="Edit User" class="btn btn-primary btn-lg ">
-                                </div>
-                              </div>
-					 </div>
-					 <!-- -->
+                    </div>
+                </div>
+                <!-- -->
 
-					 <!-- -->
-				 </form>
+                <!-- -->
+            </form>
 
-           </div>
-           <!-- /.row -->
-
-       </div>
-       <!-- /.container-fluid -->
+        </div>
+        <!-- /.row -->
 
     </div>
-    <!-- /#page-wrapper -->
-	<script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
-  <script>tinymce.init({ selector:'textarea' });</script>
-<script type="text/javascript">
+    <!-- /.container-fluid -->
 
-</script>
+</div>
+<!-- /#page-wrapper -->
+
+
+
+
 <?php include("includes/footer.php"); ?>
--- gallery/admin/includes/footer.php
+++ gallery/admin/includes/footer.php
@@ -1,38 +1,51 @@
   </div>
-    <!-- /#wrapper -->
+  <!-- /#wrapper -->
 
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+  <!-- jQuery -->
+  <script src="js/jquery.js"></script>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-    <script type="text/javascript">
-          google.charts.load('current', {'packages':['corechart']});
-          google.charts.setOnLoadCallback(drawChart);
+  <!-- Bootstrap Core JavaScript -->
+  <script src="js/bootstrap.min.js"></script>
+  <script type="text/javascript">
+      $(document).on("click",".openmodal",function() {
+        $("#photo-library").modal();
+      });
+  </script>
+  <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+  <script>
+      tinymce.init({
+          selector: 'textarea'
+      });
+  </script>
+  <script type="text/javascript">
+      google.charts.load('current', {
+          'packages': ['corechart']
+      });
+      google.charts.setOnLoadCallback(drawChart);
 
-          function drawChart() {
+      function drawChart() {
 
-            var data = google.visualization.arrayToDataTable([
+          var data = google.visualization.arrayToDataTable([
               ['Task', 'Hours per Day'],
-              ['Views',     <?php echo $session->count; ?>],
-              ['Photos',      <?php echo Photo::count_all(); ?>],
-              ['Users',  <?php echo Users::count_all(); ?>],
+              ['Views', <?php echo $session->count; ?>],
+              ['Photos', <?php echo Photo::count_all(); ?>],
+              ['Users', <?php echo Users::count_all(); ?>],
               ['Comments', <?php echo Comment::count_all(); ?>]
-            ]);
+          ]);
 
-            var options = {
-                legend:"none",
+          var options = {
+              legend: "none",
               title: 'My Daily Activities',
-              pieSliceText:'label',
-              backgroundColor:'transparent'
-            };
+              pieSliceText: 'label',
+              backgroundColor: 'transparent'
+          };
 
-            var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+          var chart = new google.visualization.PieChart(document.getElementById('piechart'));
 
-            chart.draw(data, options);
-          }
-        </script>
-</body>
+          chart.draw(data, options);
+      }
+  </script>
+  </body>
 
-</html>
-<?php ob_end_flush(); ?>
+  </html>
+  <?php ob_end_flush(); ?>
