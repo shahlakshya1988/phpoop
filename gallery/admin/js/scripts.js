c57f06670390ecc1c4d16a1ee65052fd1abdd55e
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-10-20 16:28:54 +0530

    197. Pulling Image Name with jQuery and Javascript

--- gallery/admin/js/scripts.js
+++ gallery/admin/js/scripts.js
@@ -6,4 +6,9 @@ $(document).on("click",".modal_thumbnails",function(){
     $("#set_user_image").prop("disabled",false);
     //alert($("a#user_id").data("user-id"));
     var user_id = $("a#user_id").data("user-id");
+
+    var image_href = $(this).prop("src");
+    var image_href_split = image_href.split("/");
+    var image_name = image_href_split[image_href_split.length - 1];
+    alert(image_name);
 });
