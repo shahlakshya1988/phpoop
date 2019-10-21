5fb755f9d6cbbf1c73577dd597234681b5343071
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-10-21 07:18:58 +0530

    210. Installing a Multiple Upload and Drop JS File Plugin Part #1

--- gallery/admin/includes/header.php
+++ gallery/admin/includes/header.php
@@ -27,7 +27,12 @@
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-	<!--DEVELOPER CSS -->
+	
+    <!--DROP ZONE  -->
+    <link rel="stylesheet" href="css/dropzone.css">
+    <!--DROP ZONE  -->
+
+    <!--DEVELOPER CSS -->
 	<link href="css/styles.css" rel="stylesheet">
 	<!-- DEVELOPER CSS -->
 
