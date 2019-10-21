d882667f9b675f95db287c693ea17670bd28d2f0
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-10-21 08:59:26 +0530

    223. Database refactoring

--- gallery/admin/includes/database.php
+++ gallery/admin/includes/database.php
@@ -1,9 +1,10 @@
 <?php
 class Database{
-	public $connection;
+    public $connection;
+    public $db;
 
 	public function __construct(){
-		$this->open_db_connection();
+		$this->db = $this->open_db_connection();
 	}
 
 	public function open_db_connection(){
@@ -20,29 +21,33 @@ class Database{
 		var_dump(DB_PASS);
 		var_dump(DB_NAME); */
 		//$this->connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
-		$this->connection = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+        $this->connection = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+        if($this->connection->connect_errno){
+            die("Connection Failed ".$this->connection->connect_error);
+        }
+        return $this->connection;
 		//var_dump($this->connection);
 	}
 
 	public function query($sql){
 		//$result = mysqli_query($this->connection,$sql);
 
-		$result = $this->connection->query($sql);
+		$result = $this->db->query($sql);
 		return $result;
 	}
 
 	public function confirmQuery($result){
 		if(!$result){
-			die($this->connection->error);
+			die($this->db->error);
 		}
 	}
 
 	public function escape_string($string){
-		return $this->connection->real_escape_string(trim($string));
+		return $this->db->real_escape_string(trim($string));
 	}
 
 	public function the_insert_id(){
-		return $this->connection->insert_id;
+		return $this->db->insert_id;
 	}
 
 
