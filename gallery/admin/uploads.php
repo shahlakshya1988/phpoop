39c1e101d41ba1914b66ff5867ae14691b311774
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2019-10-21 08:04:23 +0530

    211. Installing a Multiple Upload and Drop JS File Plugin Part #2

--- gallery/admin/uploads.php
+++ gallery/admin/uploads.php
@@ -1,54 +1,58 @@
 <?php include("includes/header.php"); ?>
-<?php 
-if(!$session->is_signed_in()){
+<?php
+if (!$session->is_signed_in()) {
     redirect("login.php");
     die();
 }
 $message = "";
-if(isset($_POST["upload"])){
+if (isset($_POST["upload"])) {
     $title = $_POST["title"];
     $file = $_FILES["file_upload"];
     $photo = new Photo();
     $photo->title = $title;
     $photo->set_file($file);
-    if($photo->save()){
+    if ($photo->save()) {
         $message = "Photo Uploaded Successfully";
-    }else{
-        $message = join("<br>",$photo->errors);
+    } else {
+        $message = join("<br>", $photo->errors);
     }
 }
 ?>
 
-    <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-       <?php  include "includes/top_nav.php"; ?>
-       <?php include "includes/side_nav.php"; ?>
-        <!-- /.navbar-collapse -->
-    </nav>
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <?php include "includes/top_nav.php"; ?>
+    <?php include "includes/side_nav.php"; ?>
+    <!-- /.navbar-collapse -->
+</nav>
 
 
 
-    <div id="page-wrapper">
+<div id="page-wrapper">
 
-       <div class="container-fluid">
+    <div class="container-fluid">
 
-           <!-- Page Heading -->
-           <div class="row">
-               <div class="col-lg-12">
-                   <h1 class="page-header">
-                       Uploads
-                   </h1>
-                   <ol class="breadcrumb">
-                       <li>
-                           <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
-                       </li>
-                       <li class="active">
-                           <i class="fa fa-file"></i> Blank Page
-                       </li>
-                   </ol>
-               </div>
-               <div class="col-lg-6">
-                   <?=$message; ?>
+        <!-- Page Heading -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                    Uploads
+                </h1>
+                <ol class="breadcrumb">
+                    <li>
+                        <i class="fa fa-dashboard"></i> <a href="index.html">Dashboard</a>
+                    </li>
+                    <li class="active">
+                        <i class="fa fa-file"></i> Blank Page
+                    </li>
+                </ol>
+            </div>
+            
+        </div>
+        <!-- /.row -->
+        <div class="row">
+        <div class="col-lg-6">
+                <?= $message; ?>
                 <form action="" method="POST" enctype="multipart/form-data">
                     <div class="form-group">
                         <input type="text" name="title" class="form-control">
@@ -60,14 +64,19 @@ if(isset($_POST["upload"])){
                         <input type="submit" value="Upload" name="upload" class="btn btn-success">
                     </div>
                 </form>
-               </div>
-           </div>
-           <!-- /.row -->
-
-       </div>
-       <!-- /.container-fluid -->
+            </div>
+        </div>
+        <!-- /.row -->
+        <div class="row">
+            <div class="col-lg-12">
+                <form action="upload" class="dropzone"></form>
+            </div>
+        </div>
 
     </div>
-    <!-- /#page-wrapper -->
+    <!-- /.container-fluid -->
+
+</div>
+<!-- /#page-wrapper -->
 
 <?php include("includes/footer.php"); ?>
